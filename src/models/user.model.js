bc13ae2f7c93a12cd36eaeb6782ae75b829357e5
AyushJ-03 <ayushjaiswal.work@gmail.com>
2026-02-27 02:12:18 +0530

    login successful

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -58,9 +58,8 @@ const userSchema = new Schema(
   }
 )
 
-userSchema.pre("save", async function (next) {
-  if(!this.isModified("password")) return next();
-
+userSchema.pre("save", async function () {
+  if(!this.isModified("password")) return; 
   this.password = await bcrypt.hash(this.password, 10)
 })
 
@@ -72,9 +71,7 @@ userSchema.methods.generateAccessToken = function(){
   return jwt.sign(
     {
       _id: this._id,
-      email: this.email,
-      username: this.username,
-      fullName: this.fullName
+      email: this.email
     },
     process.env.ACCESS_TOKEN_SECRET, 
     {
